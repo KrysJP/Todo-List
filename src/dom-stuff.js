76cd21ed2be2135b36498ff6982c6956f30b34b1
Krystian JP <krystianjp97@gmail.com>
2021-12-12 12:38:09 +0000

    added middle containers and their css

--- src/dom-stuff.js
+++ src/dom-stuff.js
@@ -1,26 +1,64 @@
 import css from "../styles/styles.css";
 
+// responsible for creating each part of the page
 class PageCreation {
   constructor(content) {
     this.contentName = content;
     this.content = document.querySelector(content);
   }
 
+  // wrapping function
   create() {
-    this.heading();
+    this.content.append(this.heading(), this.middle());
   }
 
   heading() {
-    var headingDiv = document.createElement("div");
-    var headingTitle = document.createElement("h1");
+    var div = document.createElement("div");
+    var title = document.createElement("h1");
 
-    headingDiv.classList.add("heading-div");
-    headingTitle.classList.add("heading-title");
+    div.classList.add("heading-container");
+    title.classList.add("heading-title");
 
-    headingTitle.textContent = "Todo List";
+    title.textContent = "Todo List";
 
-    headingDiv.appendChild(headingTitle);
-    this.content.append(headingDiv);
+    div.appendChild(title);
+
+    return div;
+  }
+
+  // includes projects and tasks (under heading, above footer)
+  middle() {
+    var bigDiv = document.createElement("div");
+
+    bigDiv.classList.add("middle-container");
+
+    bigDiv.append(this.projects(), this.tasks());
+
+    return bigDiv;
+  }
+  projects() {
+    // javascript to add
+
+    var div = document.createElement("div");
+    var heading = document.createElement("h2");
+
+    heading.textContent = "Projects";
+
+    div.classList.add("projects-container");
+    heading.classList.add("projects-heading");
+
+    div.append(heading);
+
+    return div;
+  }
+  tasks() {
+    // javascript to add
+
+    var div = document.createElement("div");
+
+    div.classList.add("tasks-container");
+
+    return div;
   }
 }
 

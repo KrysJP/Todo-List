09a1fc3b122cfb1541779114a9bfeca8d2081121
Krystian JP <krystianjp97@gmail.com>
2022-01-10 22:49:19 +0000

    changed config to docs

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,22 @@
 const path = require("path");
 
 module.exports = {
-  entry: "./src/index.js",
-  output: {
-    filename: "main.js",
-    path: path.resolve(__dirname, "dist"),
-  },
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
-      },
-    ],
-  },
-  mode: "development",
-  devtool: "inline-source-map",
-  devServer: {
-    static: "./dist",
-  },
+    entry: "./src/index.js",
+    output: {
+        filename: "main.js",
+        path: path.resolve(__dirname, "docs"),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
+            },
+        ],
+    },
+    mode: "development",
+    devtool: "inline-source-map",
+    devServer: {
+        static: "./docs",
+    },
 };
